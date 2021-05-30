52cade3a36cc9dbe38f31d124fd9064ee955e952
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-30 13:59:00 -0400

    get all books functionality

--- src/controllers/book.ts
+++ src/controllers/book.ts
@@ -1,13 +1,23 @@
 import { Request, Response, NextFunction} from 'express'
 import logging from '../config/logging'
+import Book from '../models/book'
 
 const NAMESPACE = 'Sample Controller'
 
 const getAllBooks = (req: Request, res: Response, next: NextFunction) => {
-  logging.info(NAMESPACE, 'Sample health check route called')
-
-  return res.status(200).json({
-    message: 'pong'
+  Book.find()
+  .exec()
+  .then(results => {
+    return res.status(200).json({
+      book: results,
+      count: results.length
+    })
+  })
+  .catch((error) => {
+    return res.status(500).json({
+      message: error.message,
+      error
+    })
   })
 }
 
